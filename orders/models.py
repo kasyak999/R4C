b433f5b23d410ad4994d07c523ef948d688d6ad3
Максим <kasyak999@yandex.ru>
2024-12-11 17:40:08 +0300

    up

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-
 from customers.models import Customer
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
-    robot_serial = models.CharField(max_length=5,blank=False, null=False)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    robot_serial = models.CharField(max_length=5, blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.customer} - {self.robot_serial}'
--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Robot
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .forms import RobotForm
+from django.utils import timezone
 
-# Create your views here.
+
+@csrf_exempt
+def get_all_data(request):
+    if request.method == 'GET':
+        result = list(Robot.objects.all().values(
+            'model', 'version', 'created'))
+        return JsonResponse(result, safe=False)
+    elif request.method == 'POST':
+        form = RobotForm(json.loads(request.body))
+        if form.is_valid():
+            robot = form.save(commit=False)
+            robot.created = timezone.now().date()
+            robot.serial = f'{robot.model}-{robot.version}'
+            robot.save()
+            return JsonResponse(
+                {'message': 'Робот успешно добавлен'}, status=201)
+        else:
+            return JsonResponse({'errors': form.errors}, status=400)
+    else:
+        return JsonResponse({'error': 'Не поддерживается'}, status=404)
