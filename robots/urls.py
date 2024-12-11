a9e52a313b9e51a02b84820cb62dc0703f2ee5e3
Максим <kasyak999@yandex.ru>
2024-12-11 22:36:16 +0300

    up

--- robots/urls.py
+++ robots/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.get_all_data, name='get_all_data'),
-    path('exel/', views.generate_robot_summary_excel, name='get_all_data'),
+    path('', views.get_all_data, name='robots'),
+    path('exel/', views.generate_robot_summary_excel, name='list_exel'),
 ]
